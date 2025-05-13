66778f948dce593df7136c35fa905bc90ba97a4c
Tobechukwu Nworgu <131604953+TOB-T@users.noreply.github.com>
2025-05-13 20:05:07 +0100

    Add project files.

---
+++ Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using DevIntel.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIntel.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public Role Role { get; set; } = Role.User;
+    }
+}
